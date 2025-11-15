40bf9a7c9f2cadc7f1038f802dc0c285cb49c330
Enzo Gaggiotti <enzo.gaggiotti@epitech.eu>
2025-11-15 13:07:32 +0100

    [ADD] init project and main function to handle backend server

--- backend/main.go
+++ backend/main.go
@@ -1,18 +1,60 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"log"
 
-	"github.com/joho/godotenv"
+	"backend/api"
+	"backend/api/handlers"
+	"backend/config"
+	"backend/internal/repository"
+	"backend/internal/services"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		fmt.Println("Error loading .env file")
-		return
+	config, error := config.LoadConfig()
+	if error != nil {
+		log.Fatalf("Error loading config: %v", error)
 	}
-	url := os.Getenv("BACKEND_URL")
-	fmt.Println("Backend URL:", url)
+
+	// Initialize database pool
+	pool, error := repository.NewDbPool(*config)
+	if error != nil {
+		log.Fatalf("Error creating database pool: %v", error)
+	}
+	defer pool.Close()
+
+	// Initialize repositories and services
+	contactRepo := repository.NewContactRepository(pool)
+
+	emailService := services.NewSMTPService(
+		config.SmtpHost,
+		config.SmtpPort,
+		config.SmtpUser,
+		config.SmtpPass,
+		config.AdminEmail,
+	)
+
+	// Initialize handlers
+	contactService := services.NewContactService(contactRepo, emailService)
+	contactHandler := handlers.NewContactHandler(contactService)
+
+	router := gin.Default()
+	router.SetTrustedProxies([]string{"192.168.100.59", "127.0.0.1"})
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{config.FrontendURL, config.FrontendURL_Dev},
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		AllowCredentials: true,
+	}))
+
+	api.RegisterRoutes(router, contactHandler)
+
+	log.Printf("Starting server on port %s...", config.Port)
+	if error := router.Run(":" + config.Port); error != nil {
+		log.Fatalf("Error starting server: %v", error)
+	}
 }
