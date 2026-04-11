4e7583d36806995384c92c428753ba6e8a0e05f5
Enzo Gaggiotti <enzo.gaggiotti@epitech.eu>
2026-04-11 19:24:07 +0200

    [FIX] Explicitly point Babel resolution to frontend node_modules

--- config/babel.config.js
+++ config/babel.config.js
@@ -1,7 +1,11 @@
+const path = require('path');
+
 module.exports = {
   presets: [
     [
-      require.resolve('@babel/preset-env'),
+      require.resolve('@babel/preset-env', {
+        paths: [path.join(__dirname, '../frontend')],
+      }),
       {
         targets: {
           node: 'current',
